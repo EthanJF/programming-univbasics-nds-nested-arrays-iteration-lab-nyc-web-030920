6d76a9771b8f9b7d3ba97a054c08cad141c01c29
Ethan Fournier <ethanjosephfournier@gmail.com>
2020-03-04 16:07:50 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -23,7 +23,7 @@ def find_greater_pair(src)
   result = []
   
   while i < src.length do
-    (src[i][0] > src[i][1]) ? result << src[i][0] : result << src[i][1]
+    src[i][0] > src[i][1] ? result << src[i][0] : result << src[i][1]
     i += 1
   end
   result

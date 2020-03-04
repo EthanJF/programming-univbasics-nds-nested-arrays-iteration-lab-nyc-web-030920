c70a9e3367b1b92483fcb8f86e4066554f22890e
Ethan Fournier <ethanjosephfournier@gmail.com>
2020-03-04 15:50:12 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -14,16 +14,7 @@ def join_ingredients(src)
     row_index += 1
   end
   resulting_array
-  #   result = []
 
-  # i = 0
-  # while i < src.length do
-  #   inner = src[i]
-  #   result << "I love #{inner[0]} and #{inner[1]} on my pizza"
-  #   i += 1
-  # end
-
-  # result
 end
 
 def find_greater_pair(src)
